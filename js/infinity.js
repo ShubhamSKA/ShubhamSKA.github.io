61bd5717cc6cfcfe195b1789bdba390afce3a147
ShubhamSKA <skumarag@purdue.edu>
2026-02-03 12:17:43 -0500

    fixed typo

--- js/infinity.js
+++ js/infinity.js
@@ -1,4 +1,4 @@
-i const canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 const body2 = document.querySelector("body");
 body2.style.overflowY = "hidden";
@@ -30,6 +30,7 @@ function handleResize() {
 }
 window.addEventListener("resize", handleResize);
 window.addEventListener("load", handleResize); 
+
 let opacity = 0;
 let overall_opacity = 1;
 let start_explosion = 0;
